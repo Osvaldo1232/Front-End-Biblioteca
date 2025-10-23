e5f9ed18ae6d918d259a14e810e28a630e4a0e75
Osvaldo1232 <florencioosvaldo49@gmail.com>
2025-10-22 19:13:25 -0600

    se sube inicio de sesion
    se sube inicio de sesion

--- Biblioteca-Utsem/src/app/home/home.page.ts
+++ Biblioteca-Utsem/src/app/home/home.page.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule  // <-- esto permite usar ion-label, ion-input, ion-button, etc.
+  ],
+
 })
 export class HomePage {
+
+  usuario: string = '';
+  password: string = '';
+
   constructor() {}
+
+  login() {
+    console.log('Usuario:', this.usuario, 'Contraseña:', this.password);
+    // Aquí llamas a tu servicio de autenticación
+    // Ejemplo: this.authService.login(this.usuario, this.password)
+  }
+
+  
 }
